d3fe216b3853c9f844cd24fc251d2378a37f95dc
akshayc11 <akshayc@andrew.cmu.edu>
2013-09-16 19:45:33 -0700

    obj_reference_domain.py Object Reference vs Domain breakdown done. Few errors remain to be addressed in the annotations, but the results look pretty reasonable

--- obj_reference_domain.py
+++ obj_reference_domain.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import read_write_annotation_files as rw
 import annotation_schema
+import sys
 read_complex  =  rw.read_annotation_file
 read_simple   =  rw.read_simple_annotation_file
 write_complex =  rw.write_annotation_file
@@ -33,7 +34,7 @@ if __name__ == '__main__':
     
     parser.add_argument('--config',
                         type=str,
-                        default=''
+                        default='',
                         help='''
                         file where other optional configurations
                         are stored
@@ -52,23 +53,27 @@ if __name__ == '__main__':
         line = line.strip()
         runList.append(line)
     
+    domainLabels = annotation_schema.domainLabels
+    objectReferenceComplexLabels = annotation_schema.objectReferenceComplexLabels
+    
     analysisCount = dict()
-        domainList = []
-        for domainLabel in domainLabels:
-            domainName = domainLabel.name
-            domainList.append(domainName)
+    domainList = []
+    for domainLabel in domainLabels:
+        domainName = domainLabel.name
+        domainList.append(domainName)
         
-        domainList.sort()
+    domainList.sort()
+    print domainList    
+    objSet = set()
+    for objLabel in objectReferenceComplexLabels:
+        objName = (objLabel.name).split(' (')[0]
+        objSet.add(objName)
         
-        objSet = set()
-        for objLabel in objectReferenceComplexLabels:
-            objName = (objLabel.name).split(' (')
-            objSet.add(objName)
-        
-        objList =list(objSet)
-        objList.sort()
-        
-        gestureList = ['(Gesture)', '(No Gesture)']
+    objList =list(objSet)
+    objList.sort()
+    print objList
+    
+    gestureList = ['(Gesture)', '(No Gesture)']
     
     # Create the analysisCount structure
     for objName in objList:
@@ -80,16 +85,51 @@ if __name__ == '__main__':
             for domainName in domainList:
                 analysisCount[objName][gesture][domainName] = 0
             
-        
+    
     # Update analysisCount from all the runs
+    totalCount = 0
+    errCount   = 0
     for runId in runList:
+        print runId
         run               = dataDir + '/' + runId
         objectReferenceId = run + '/' + 'object-reference.xml'
-        domainId          = run + '/' + 'DA.xml'
+        domainId          = run + '/' + 'domain-annotated.xml'
         
         objWords, objAnnotations, objNotes = read_complex(objectReferenceId)
         domWords, domAnnotations, domNodes = read_simple(domainId)
         
         
-        
-        
+        for objAnnotation in objAnnotations:
+            objLabel     = objAnnotation.label
+            gesture      = ''.join(objLabel.partition('(')[1:])
+            label        = objLabel.partition('(')[0].strip()
+            objWords     = objAnnotation.words
+            domSet = set()
+            for word in objWords:
+                for domAnnotation in domAnnotations:
+                    wordNames = [wrd.name for wrd in domAnnotation.words]
+                    if word.name in wordNames:
+                        #print domAnnotation.label
+                        domSet.add(domAnnotation.label)
+                    
+                
+            
+            domList = sorted(list(domSet))
+            if len(domList) == 0:
+                domList.append('OOD')
+            #print gesture, label,domList 
+            for dom in domList:
+                try:
+                    analysisCount[label][gesture][dom] += 1
+                    totalCount += 1
+                except:
+                    errCount += 1
+                    print "error for:", dom
+    
+    for objName in objList:
+        for gesture in gestureList:
+            for domainName in domainList:
+                print objName, gesture, domainName, analysisCount[objName][gesture][domainName] 
+    
+    print 'total:', totalCount
+    print 'errors in:', errCount
